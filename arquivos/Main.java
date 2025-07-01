9d24415ff88dbc7d1397d7f3a31a73552a7f1750
Rafael Neves <rafaelnvss002@gmail.com>
2025-06-30 22:14:42 -0300

    fix again

--- arquivos/Main.java
+++
@@ -1,8 +0,0 @@
-package arquivos;
-public class Main {
-    public static void main(String[] args){
-        Triangulo triangulo = new Triangulo(3, "triangao", new float[]{3, 4, 5});
-        triangulo.mostrarArea();
-        triangulo.mostrarDimensoes();
-    }
-}
